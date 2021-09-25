1118797c8d64ed3e350533bf3deecc2f4b5e639a
Tony Zhang <tozhang665@gmail.com>
2021-09-24 23:10:25 -0700

    started components

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
+  # resources :users, only: [:new, :create]
+  # resource :session, only: [:new, :create, :destroy]
+
+   namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :destroy]
+    resources :users, only:[:create,:show]
+  end
+
 end
--- frontend/bamazon_redux.jsx
+++ frontend/bamazon_redux.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import Root from "./components/root"
 
 
 document.addEventListener("DOMContentLoaded",()=>{
   const rootEL = document.getElementById("root")
 
-  ReactDOM.render(<h1>tester</h1>,rootEL)
+  ReactDOM.render(<Root/>,rootEL)
 })
